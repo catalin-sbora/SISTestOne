31a3135799bff35adb2e5b2857c0382f7a24fe92
catalin-sbora <catalin.sbora@gmail.com>
2025-12-07 10:22:47 -0800

    Add SecurityTest API controller with ping command execution
    Added new SecurityTest controller with standard CRUD endpoints. The GET endpoint executes a ping command using Process.Start, which may introduce command injection risks if input is not validated.

---
+++ TestOneWebApp/TestOneWebApp/Controllers/SecurityTest.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TestOneWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecurityTest : ControllerBase
+    {
+        // GET: api/<SecurityTest>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<SecurityTest>/5
+        [HttpGet("{ip}")]
+        public string Get(string ip)
+        {
+            var commandToExecute = "ping -c " + ip;
+            ProcessStartInfo processStartInfo = new ProcessStartInfo("cmd.exe", "-c \"" + commandToExecute + "\"");
+            Process? process = Process.Start(processStartInfo);
+            if (process != null)
+            {
+                process.WaitForExit();
+            }
+            return commandToExecute;
+        }
+
+        // POST api/<SecurityTest>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<SecurityTest>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<SecurityTest>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
