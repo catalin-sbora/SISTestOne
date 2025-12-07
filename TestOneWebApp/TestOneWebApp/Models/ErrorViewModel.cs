b4db2ddd583ce94952417d3d04ad2e750e79791c
catalin-sbora <catalin.sbora@gmail.com>
2025-12-07 09:43:34 -0800

    Create initial project

---
+++ TestOneWebApp/TestOneWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestOneWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
